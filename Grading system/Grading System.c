6b5a464eac50bf2f7813b7b3b659596747b956de
Tonmoy Roy Rony <rtonmoy427@gmail.com>
2024-11-14 22:43:15 +0600

    SL

--- Grading system/Grading System.c
+++ Grading system/Grading System.c
@@ -2,14 +2,20 @@
 
 int calculateGrade(int marks) {
     if (marks >= 90) return 'A';
-    else if (marks >= 80) return 'B';
-    else if (marks >= 70) return 'C';
-    else if (marks >= 60) return 'D';
-    else if (marks >= 33) return 'E';
-    else return 'F';
+    else if (marks >= 80)
+        return 'B';
+    else if (marks >= 70)
+     return 'C';
+    else if (marks >= 60)
+     return 'D';
+    else if (marks >= 33)
+     return 'E';
+    else
+     return 'F';
 }
 
-int main() {
+int main()
+{
     int bangla, english, math, physics, chemistry, biology;
     float average;
 
