df5ce54814011a357131e7bdcc513367dc5b9a87
Tonmoy Roy Rony <rtonmoy427@gmail.com>
2025-02-28 00:48:12 +0600

    project

---
+++ Tonmoy.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Tonmoy
+{
+	public static void main(String[]args)
+	{
+		System.out.println("This is Tonmoy's first java project");
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter an integer value:");
+		int value1 = s1.nextInt();
+		
+		Scanner s2 = new Scanner(System.in);
+		System.out.println("Enter your double valu");
+		double value2 = s2.nextDouble();
+		System .out.println("This your double value:" + value2);
+				System.out.println("This is your entered value:" + value1);
+
+	}
+}
