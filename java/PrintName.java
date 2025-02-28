f306330db8fddf4cc6098535bdc65a939b68e990
Tonmoy Roy Rony <rtonmoy427@gmail.com>
2025-02-28 21:27:59 +0600

    PrintName

---
+++ java/PrintName.java
@@ -0,0 +1,22 @@
+
+
+import java.lang.*;
+import java.util.Scanner;
+publice class  PrintName
+{   
+    public static void main(String[]args){
+
+    
+    Scanner s1 = new Scanner(System.in);
+    System.out.println("Enter your name:");
+    String name = s1. nextLine();
+    System.out.println("This is your name:"+ name)
+    s1.close();
+
+
+
+    }
+
+
+
+}
